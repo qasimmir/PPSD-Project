810fce9b39db254caf0d8c082b4d922c7af7f111
harisali123 <khharis66@gmail.com>
2019-12-16 11:47:14 -0800

    transfer function added

--- atm.cpp
+++ atm.cpp
@@ -6,71 +6,77 @@ using namespace std;
 class ATM
 {
 private:
-    int value, index;
+	int value, index;
 	int amount, convert;
 	string  pin,input;
 	string arr[2][3];
 
-
 public:
 
-	void load_file();
-	void save_to_file();
-	void cash_withdrawal();
-	void cash_deposit();
-	void check_balance();
-	void change_pin();
-
-};
-void ATM::load_file()
-{
-	fstream file;
-	file.open("C:\\Users\\Qasim Mir\\Desktop\\data.txt");
-	if (file.is_open())
+	void load_file()
 	{
-		file >> input;
-		while (!file.eof())
+		fstream file;
+		file.open("C:\\Users\\Qasim Mir\\Desktop\\data.txt");
+		if (file.is_open())
+		{
+			file >> input;
+			while (!file.eof())
+			{
+				for (int i = 0; i < 2; i++)
+				{
+					for (int j = 0; j < 3; j++)
+					{
+						arr[i][j] = input;
+						file >> input;
+					}
+				}
+			}
+		}
+		else
+			cout << "File is unable to open" << endl << endl;
+		file.close();
+
+	}
+
+	void save_to_file()
+	{
+		fstream file;
+		file.open("C:\\Users\\Qasim Mir\\Desktop\\data.txt", ios::out | ios::trunc);
+
+		if (file.is_open())
 		{
 			for (int i = 0; i < 2; i++)
 			{
 				for (int j = 0; j < 3; j++)
 				{
-					arr[i][j] = input;
-					file >> input;
+					file << arr[i][j] << endl;
 				}
+				file << endl;
 			}
-		}
-	}
-	else
-		cout << "File is unable to open" << endl << endl;
-	file.close();
-}
-void ATM::save_to_file()
-{
-	fstream file;
-	file.open("C:\\Users\\Qasim Mir\\Desktop\\data.txt", ios::out | ios::trunc);
+			cout << "Data sent to file.." << endl << endl;
 
-	if (file.is_open())
+		}
+		else
+			cout << "File is not able to open";
+
+		file.close();
+	}
+
+	void display()
 	{
 		for (int i = 0; i < 2; i++)
 		{
 			for (int j = 0; j < 3; j++)
 			{
-				file << arr[i][j] << endl;
+				cout << arr[i][j] << endl;
 			}
-			file << endl;
+			cout << endl;
 		}
-		cout << "Data sent to file.." << endl << endl;
-
 	}
-	else
-		cout << "File is not able to open";
 
-	file.close();
-}
-void ATM::cash_withdrawal(string account_no)
-{
 
+	void cash_withdrawal(string account_no)
+	{
 		for (int i = 0; i < 2; i++)
 		{
 			int j = 0;
@@ -104,16 +110,17 @@ void ATM::cash_withdrawal(string account_no)
 				}
 
 			}
+			//There are some problems in this else.. it wroks everytime when we are finding acc. no
 			else
 			{
 				cout << "Invalid Account No." << endl;
 			}
 		}
+	}
 
-}
-void ATM::cash_deposit(string account_no)
-{
-    		for (int i = 0; i < 2; i++)
+	void cash_deposit(string account_no)
+	{
+		for (int i = 0; i < 2; i++)
 		{
 			int j = 0;
 			if (arr[i][j] == account_no)
@@ -138,16 +145,17 @@ void ATM::cash_deposit(string account_no)
 				arr[i][j] = to_string(amount);
 
 			}
+			//There are some problems in this else.. it wroks everytime when we are finding acc. no
 			else
 			{
 				cout << "Invalid Account No." << endl;
 			}
 		}
+	}
 
-}
-void ATM::check_balance()
-{
-for (int i = 0; i < 2; i++)
+	void check_balance(string account_no)
+	{
+		for (int i = 0; i < 2; i++)
 		{
 			int j = 0;
 			if (arr[i][j] == account_no)
@@ -174,11 +182,11 @@ for (int i = 0; i < 2; i++)
 				cout << "Invalid Account No." << endl;
 			}
 		}
-}
+	}
 
-void ATM::change_pin()
-{
-for (int i = 0; i < 2; i++)
+	void change_pin(string account_no)
+	{
+		for (int i = 0; i < 2; i++)
 		{
 			int j = 0;
 			if (arr[i][j] == account_no)
@@ -205,16 +213,69 @@ for (int i = 0; i < 2; i++)
 				cout << "Invalid Account No." << endl;
 			}
 		}
-}
+	}
+	 void Transfer_amount(string account_no)
+     {
+        for (int i = 0; i < 2; i++)
+     {
+    int j = 0;
+    if (arr[i][j] == account_no)
+    {
+    j++;
+    cout << "Enter your PIN:";
+    cin >> pin;
+
+    while (pin != arr[i][j])
+    {
+    cout << "The Pin you Entered was Wrong." << endl;
+    cout << "Press Q to Quit.";
+    cout << "Enter Pin again:";
+    cin >> pin;
+    }
+
+    cout << "Enter account number in which you want to transfer:";
+    cin >> transfer;
+
+    j++;
+    cout << "Enter the amount you want to transfer:";
+    cin >> amount;
+
+    convert = stoi(arr[i][j]);
+
+    if (convert >= amount)
+    {
+    T_amount = amount;
+    amount = convert - amount;
+    arr[i][j] = to_string(amount);
+    }
+    else
+    {
+    cout << "You have insufficient balance for this Transfer.";
+    return;
+    }
+
+
+    }
+    else
+    {
+    cout << "Invalid Account No." << endl;
+    }
+    }
+    }
+
+};
+
+
 int main()
 {
 	ATM obj;
 	char option, repeat;
-    string account_no;
+	string account_no;
 
-    obj.load_file();
+	obj.load_file();
+	cout << endl << endl;
 
-	do{
+	do {
 		cout << "Enter W to Withdraw Cash" << endl;
 		cout << "Enter D to deposit Cash" << endl;
 		cout << "Enter B to check balance" << endl;
@@ -223,19 +284,22 @@ int main()
 		cin >> option;
 		cout << endl;
 
-
 		if (option == 'W' || option == 'w')
 		{
 			cout << "Enter your Account NO.:";
 			cin >> account_no;
 			obj.cash_withdrawal(account_no);
+			cout << endl;
+			obj.display();
 			obj.save_to_file();
 		}
+
 		if (option == 'D' || option == 'd')
 		{
 			cout << "Enter your Account NO.:";
 			cin >> account_no;
 			obj.cash_deposit(account_no);
+			obj.display();
 			obj.save_to_file();
 		}
 
@@ -244,7 +308,7 @@ int main()
 			cout << "Enter your Account NO.:";
 			cin >> account_no;
 			obj.check_balance(account_no);
-
+			obj.display();
 			obj.save_to_file();
 		}
 
@@ -253,18 +317,13 @@ int main()
 			cout << "Enter your Account NO.:";
 			cin >> account_no;
 			obj.change_pin(account_no);
+			obj.display();
 			obj.save_to_file();
 		}
 
-		cout << endl << "If you want to continue with another activity than press C:";
 
+		cout << endl << "If you want to continue with another activity than press C:";
 		cin >> repeat;
 
 	} while (repeat == 'C' || repeat == 'c');
-
-
-
-
-	return 0;
 }
-
