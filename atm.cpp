e6eb7eba0a7cb136e52f9238c41333b6e86b232f
qasimmir <qasim2018@namal.edu.pk>
2019-12-16 11:46:18 +0500

    Main updated

--- atm.cpp
+++ atm.cpp
@@ -9,21 +9,33 @@ private:
 
 public:
 
-    	void cash_withdrawal();
-    	void cash_deposit();
-    	void check_balance();
-    	void change_pin();
+	void cash_withdrawal();
+	void cash_deposit();
+	void check_balance();
+	void change_pin();
 
 };
 
 int main()
 {
-    ATM obj;
+	ATM obj;
+	char option, repeat;
 
-    return 0;
+
+	do {
+		cout << "Enter W to Withdraw Cash" << endl;
+		cout << "Enter D to deposit Cash" << endl;
+		cout << "Enter B to check balance" << endl;
+		cout << "Enter P to change your PIN" << endl;
+		cout << endl << "Enter Here:";
+		cin >> option;
+		cout << endl;
+
+		cout << endl << "If you want to continue with another activity than press C:";
+		cin >> repeat;
+
+	} while (repeat == 'C' || repeat == 'c');
+
+	return 0;
 }
-//upfsty
-
-
-//new aa gaya
 
